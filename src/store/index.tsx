8a68ce2c18a1c4c30929577ef729b8479fc4abac
4iTeRiNo <zubkovigor@mail.ru>
2023-09-20 22:12:47 +0200

    создал rootReducers

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import rootReducer from "./rootReducer";
+
+
 const store = configureStore({
-    reducer: {},
+    reducer: rootReducer,
 })
 
 export default store;
