5f11b9f33d15ed16b761624b6faa00beba54cba4
4iTeRiNo <zubkovigor@mail.ru>
2023-09-22 12:35:18 +0200

    установил и подключил normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import { Provider } from 'react-redux'
-import App from './App.tsx'
 
+import './main.css'
+import 'normalize.css'
+
+import App from './App.tsx'
 import store from './store/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
