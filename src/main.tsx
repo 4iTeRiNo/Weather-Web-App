620f7c2eb51ccad82532c517d3ec890d7b85c282
4iTeRiNo <zubkovigor@mail.ru>
2023-09-25 15:13:31 +0200

    Заменил svg иконки Заменил обложки карточек Создал элементы: weatherCard, navigatePanel.

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
-import './main.css'
 import 'normalize.css'
+import './main.css'
 
 import App from './App.tsx'
 import store from './store/index.tsx'
