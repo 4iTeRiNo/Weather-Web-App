d87bff71ecc639b80a393d5e1a43443e66aece00
4iTeRiNo <zubkovigor@mail.ru>
2023-09-26 18:42:19 +0200

    Navigation изменил базовый цвет svg изменил путь импорта svg

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,45 +1,44 @@
 import styles from './Navigation.module.css';
 
-import Statistic from '../../assets/svg/navigate/statistic.svg';
-import Stats from '../../assets/svg/navigate/stats-chart-outline.svg';
-import Point from '../../assets/svg/navigate/point.svg';
-import Calendar from '../../assets/svg/navigate/calendar-outline.svg';
-import Settings from '../../assets/svg/sunrise.svg';
-import LogInOut from '../../assets/svg/sunrise.svg';
+import Statistic from '../../assets/svg/navigate/statistic.svg?react';
+import Stats from '../../assets/svg/navigate/stats-chart-outline.svg?react';
+import Point from '../../assets/svg/navigate/point.svg?react';
+import Calendar from '../../assets/svg/navigate/calendar-outline.svg?react';
+import Settings from '../../assets/svg/navigate/settings.svg?react';
+import LogInOut from '../../assets/svg/navigate/gg_log-off.svg?react';
 
 
 // interface NavigationProps { }
 
 export const Navigation = () => {
-  return(
+  return (
 
-  
-  <div className={styles.navigation}>
-    <nav className="navigate">
-      <ul className="linkItems">
-        <li className="linkItem">
-          <Statistic />
-        </li>
-        <li className="linkItem">
-          <Stats />
-        </li>
-        <li className="linkItem">
-          <Calendar />
-        </li>
-        <li className="linkItem">
-          <Point />
-        </li>
-        <li className="linkItem">
-          <Settings />
-        </li>
-      </ul>
+    <div className={styles.navigation}>
+      <nav className="navigate">
+        <ul className="linkItems">
+          <li className="linkItem">
+            <Statistic />
+          </li>
+          <li className="linkItem">
+            <Stats />
+          </li>
+          <li className="linkItem">
+            <Calendar />
+          </li>
+          <li className="linkItem">
+            <Point />
+          </li>
+          <li className="linkItem">
+            <Settings />
+          </li>
+        </ul>
 
-      <ul className="linkItems">
-        <li className="linkItem">
-          <LogInOut />
-        </li>
-      </ul>
-    </nav>
-  </div>
+        <ul className="linkItems">
+          <li className="linkItem">
+            <LogInOut />
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
-  };
+};
