9791e0159154cdfd1f43a4765aaf30b49fb715a4
4iTeRiNo <zubkovigor@mail.ru>
2023-09-26 18:28:44 +0200

    создал компонент Header изменил название svg

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,45 @@
 import styles from './Header.module.css';
 
-import { SearchIcon, NotificationIcon } from '../SGVIcons';
+import {SearchIcon, NotificationIcon} from '../SGVIcons';
 
 // interface HeaderProps { }
 
 export const Header = () => {
-  return (
-
-    <header className={styles.header}>
-      <section className={styles.intro}>
-        <div>
-          <img src="#" alt="#" />
-        </div>
-        <div className={styles.user}>
-          <h1>Hello,</h1>
-          <span className={styles.name}>Username</span>
-        </div>
-        <form autoComplete='on' className={styles.searchForm}>
-          <div className={styles.search}>
-            <input type="text" className={styles.text} name="cityName" id="search" placeholder="Search city" />
-            <label htmlFor="search" className="label">
-              <SearchIcon />
-            </label>
-          </div>
-          <NotificationIcon />
-        </form>
-      </section>
-    </header>
-
-  )
+    return (
+        <header className={styles.header}>
+            <section className={styles.intro}>
+                <div>
+                    <img
+                        src='#'
+                        alt='#'
+                    />
+                </div>
+                <div className={styles.user}>
+                    <h1>Hello,</h1>
+                    <span className={styles.name}>Username</span>
+                </div>
+                <form
+                    autoComplete='on'
+                    className={styles.searchForm}
+                >
+                    <div className={styles.search}>
+                        <input
+                            type='text'
+                            className={styles.text}
+                            name='cityName'
+                            id='search'
+                            placeholder='Search city'
+                        />
+                        <label
+                            htmlFor='search'
+                            className='label'
+                        >
+                            <SearchIcon />
+                        </label>
+                    </div>
+                    <NotificationIcon />
+                </form>
+            </section>
+        </header>
+    );
 };
