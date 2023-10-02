3681b1d002d33705db462eeddf8ae1976b427c46
4iTeRiNo <zubkovigor@mail.ru>
2023-10-02 17:10:40 +0200

    создал компонент RadiationUVI исправил ошибку в названии root стилей

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,22 +2,25 @@ import styles from './Footer.module.css';
 import { Button } from '../Button';
 import { GraphSun } from '../GraphSun';
 import { WeatherPrediction } from '../WeatherPrediction';
+import { RadiationUVI } from '../RadiationUVI';
 
 // interface FooterProps { }
 
 export const Footer = () => {
   return (
-  
-  <footer className={styles.footer}>
-     <div className={styles.currentPoint}>
-      <div className={styles.city}>
-        <span className={styles.weather}>Sun</span>
-        <span className={styles.currentCity}>Boston, UA</span>
+
+    <footer className={styles.footer}>
+      <div className={styles.currentPoint}>
+        <div className={styles.city}>
+          <span className={styles.weather}>Sun</span>
+          <span className={styles.currentCity}>Boston, UA</span>
+        </div>
+        <span className={styles.temp}>22&#176;С</span>
       </div>
-      <span className={styles.temp}>22&#176;С</span>
-    </div>
-    <GraphSun/>
-    <WeatherPrediction/>
-    <Button/>
-  </footer>
-)};
+      <GraphSun />
+      <RadiationUVI />
+      <WeatherPrediction />
+      <Button />
+    </footer>
+  )
+};
