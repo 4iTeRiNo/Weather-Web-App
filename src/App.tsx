bd4140fcdcb2cb564a3cd170de81a469b41c7b7d
4iTeRiNo <zubkovigor@mail.ru>
2023-10-03 19:51:58 +0200

    изменил позиционирование компонентов в грид сетке; добавил стили компонентам; довил svg avatarIcon;

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
 import { Main } from "./components/Main"
+import { Navigation } from "./components/Navigation"
 
 function App() {
   return (
     <>
     <Header/>
+    <Navigation/>
     <Main/>
     <Footer/>
     </>
