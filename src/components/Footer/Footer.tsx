a49b2d46038c4df4120dd3023775d5068e17ed75
4iTeRiNo <zubkovigor@mail.ru>
2023-10-17 17:38:46 +0200

    вынес в отдельный компонент фунуцию

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,28 +4,12 @@ import {Button} from '../Button';
 import {GraphSun} from '../GraphSun';
 import {RadiationUVI} from '../RadiationUVI';
 import {WeatherPrediction} from '../WeatherPrediction';
-import {useAppSelector} from '../../hooks';
-
-// interface FooterProps { }
+import { WeatherCity } from './WeatherCity';
 
 export const Footer = () => {
-  const weatherValue = useAppSelector((state) => state?.defaultCities.list);
-
-  const weatherCity = weatherValue.map((value, index) => {
-    return (
-      <div key={index} className={styles.currentPoint}>
-        <div className={styles.city}>
-          <span className={styles.weather}>{value.current.condition.text}</span>
-          <span className={styles.weather}>{value.location.tz_id}</span>
-        </div>
-        <span className={styles.temp}>{value.current.temp_c}&#176;C</span>
-      </div>
-    );
-  });
-
-  return (
+return (
     <footer className={styles.footer}>
-      {weatherCity}
+      <WeatherCity/>
       <GraphSun />
       <RadiationUVI />
       <WeatherPrediction />
