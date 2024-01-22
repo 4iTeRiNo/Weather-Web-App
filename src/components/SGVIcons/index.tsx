bbb4990374cee7da9ed84d7c9b1774378b7442cc
Zubkov Igor <zubkovigor@mail.ru>
2024-01-22 11:53:47 +0200

    Исправил resolve картинки

--- src/components/SGVIcons/index.tsx
+++ src/components/SGVIcons/index.tsx
@@ -1,7 +1,6 @@
 //Icons
 export {default as SunSetIcon} from '../../assets/svg/sunset.svg?react';
 export {default as CloudyIcon} from '../../assets/cloudy/cloudyIcon.svg?react';
-
 export {default as SunRiseIcon} from '../../assets/svg/sunrise.svg?react';
 export {default as ThermometerIcon} from '../../assets/svg/thermometer.svg?react';
 export {default as Pressure} from '../../assets/svg/pressure.svg?react';
@@ -9,7 +8,7 @@ export {default as WindyIcon} from '../../assets/svg/windyIcon.svg?react';
 export {default as AvatarICon} from '../../assets/user/avatar.svg?react';
 
 // Background
-export {default as PartlyCloudy} from '../../assets/cloudy/partlyCloudy.svg';
+export {default as PartlyCloudy} from '../../assets/cloudy/partlyCloudy.svg?react';
 export {default as Sunny} from '../../assets/sunny/sunny.jpeg';
 export {default as Cloudy} from '../../assets/cloudy/cloudy.svg';
 export {default as Snowy} from '../../assets/snowy/snowy.jpeg';
